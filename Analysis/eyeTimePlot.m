2fe539364de3bd6a2e489f583f18fcdb3e7bbaf8
lovegroove <erichart@dhcp-128-83-97-66.psy.utexas.edu>
2014-12-03 14:11:06 -0600

    minor analysis update

--- Analysis/eyeTimePlot.m
+++ Analysis/eyeTimePlot.m
@@ -10,7 +10,7 @@ for k = 1:length(PDS.eyepos)-6 % currently just doing first 12
     xlim([0 dv.disp.winRect(3)])
     ylim([0 dv.disp.winRect(4)])
     for i = 1:length(PDS.eyepos{k})
-        hold on
+        hold on;
         subplot(4,(length(PDS.eyepos)-6)/4,k), plot(PDS.eyepos{k}(i,1),PDS.eyepos{k}(i,2),'g--o','LineWidth',2)
     end
     
