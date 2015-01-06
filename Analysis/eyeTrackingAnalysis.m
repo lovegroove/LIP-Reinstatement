3b4e346ebd03ae4f9f12a61470428411bb1a755b
lovegroove <ehart004@gmail.com>
2015-01-06 14:35:43 -0600

    eyetracking analysis update

---
+++ Analysis/eyeTrackingAnalysis.m
@@ -0,0 +1,133 @@
+% Analyze eye tracking data
+% PDS.eyepos = [x,y,trialTime, trialState], each cell from a trial
+% PDS.data.eyeposloop is the same i think
+% dv.states.SHOWPROBE = 11;
+
+format longg
+
+eyePos = PDS.eyepos(strcmp(PDS.trialType,'test'));
+objLocs = PDS.data.objectLocs(strcmp(PDS.trialType,'test'));
+
+state = dv.states.SHOWPROBE; %only the eyePos during memory probe
+
+% NOTE: Double check that the objlocs always have the correct target first in the array!!!!!!!!!!!!!!!!!!!! 
+
+n = length(eyePos);
+eyePosState = cell(1,n);
+eyePosProp = cell(1,n);
+emptyCells = zeros(n,1);
+firstSaccadeSample = zeros(n,1);
+firstSaccadeTimeInState = zeros(n,1);
+firstSaccadeStatus = zeros(n,1);
+relativeMatch = zeros(n,1);
+relativeMatchTime = zeros(n,1);
+
+% save these as awe go???
+prop1 = zeros(n,1);
+prop2 = zeros(n,1);
+prop0 = zeros(n,1);
+prop1Time = zeros(n,1);
+prop2Time = zeros(n,1);
+prop0Time = zeros(n,1);
+
+for iTrial = 1:n
+    
+    eyePosState{iTrial} = eyePos{iTrial}(eyePos{iTrial}(:,4)==state,:); % only eye position during a certain state
+    sampleLen = length(eyePosState{iTrial});
+    
+    for jSample = 1:sampleLen
+        % eyes within correct object boundaries
+        if eyePosState{iTrial}(jSample,1) >= objLocs{iTrial}{2,1}(1,1) && eyePosState{iTrial}(jSample,1) <= objLocs{iTrial}{2,1}(1,3) && eyePosState{iTrial}(jSample,2) >= objLocs{iTrial}{2,1}(1,2) && eyePosState{iTrial}(jSample,2) <= objLocs{iTrial}{2,1}(1,4)
+            
+        eyePosProp{iTrial}(jSample,1) = 1;    
+            
+        % eyes within foil object boundaries
+        elseif eyePosState{iTrial}(jSample,1) >= objLocs{iTrial}{2,2}(1,1) && eyePosState{iTrial}(jSample,1) <= objLocs{iTrial}{2,2}(1,3) && eyePosState{iTrial}(jSample,2) >= objLocs{iTrial}{2,2}(1,2) && eyePosState{iTrial}(jSample,2) <= objLocs{iTrial}{2,2}(1,4)
+            
+        eyePosProp{iTrial}(jSample,1) = 2;  
+        
+        % eyes in neither
+        else
+        eyePosProp{iTrial}(jSample,1) = 0;  
+        
+        end
+    end
+    
+    % flag empty cells due to break fixation
+    emptyCells(iTrial) = isempty(eyePosProp{iTrial});
+    
+    
+    if ~isempty(eyePosProp{iTrial})
+        % first saccade
+        firstSaccadeSample(iTrial) = find(eyePosProp{iTrial} ~= 0,1);
+        % time of first saccade
+        eyePosStateTime = eyePosState{iTrial}(:,3);
+        firstSaccadeTimeInState(iTrial) = eyePosStateTime(firstSaccadeSample(iTrial)) - eyePosStateTime(1);
+        % status of first saccade (location - target or foil)
+        firstSaccadeStatus(iTrial) = eyePosProp{iTrial}(firstSaccadeSample(iTrial)); 
+        
+        % relative viewing
+        prop1(iTrial) = length(find(eyePosProp{iTrial} == 1)) / length(eyePosProp{iTrial});
+        prop2(iTrial) = length(find(eyePosProp{iTrial} == 2)) / length(eyePosProp{iTrial});
+        prop0(iTrial) = length(find(eyePosProp{iTrial} == 0)) / length(eyePosProp{iTrial});
+        
+        sampleTime = (eyePosStateTime(end) - eyePosStateTime(1)) / length(eyePosStateTime);
+        prop1Time(iTrial) = sampleTime * length(find(eyePosProp{iTrial} == 1));
+        prop2Time(iTrial) = sampleTime * length(find(eyePosProp{iTrial} == 2));
+        prop0Time(iTrial) = sampleTime * length(find(eyePosProp{iTrial} == 0));
+        
+        relativeMatch(iTrial) = prop1 / (prop1 + prop2);
+        relativeMatchTime(iTrial) = prop1Time / (prop1Time + prop2Time);
+    else
+        firstSaccadeSample(iTrial) = NaN;
+        firstSaccadeTimeInState(iTrial) = NaN;
+        firstSaccadeStatus(iTrial) = NaN;
+    end
+    
+    
+end
+
+%eyePosProp = eyePosProp(~cellfun('isempty',eyePosProp));  
+
+% eliminate NaNs
+firstSaccadeSample = firstSaccadeSample(~isnan(firstSaccadeSample));
+firstSaccadeTimeInState = firstSaccadeTimeInState(~isnan(firstSaccadeTimeInState));
+firstSaccadeStatus = firstSaccadeStatus(~isnan(firstSaccadeStatus));
+
+
+
+
+%% Analysis
+
+
+% RTs
+saccadeRT = [firstSaccadeTimeInState firstSaccadeStatus];
+anova1(saccadeRT(:,1),saccadeRT(:,2))
+
+
+firstSaccadeTimeInState = sort(firstSaccadeTimeInState);  % sorting we lose what the 'status' is
+firstSaccadeTimeInState1 = firstSaccadeTimeInState(1:end/2);
+firstSaccadeTimeInState2 = firstSaccadeTimeInState(end/2+1:end);
+
+
+%% logistic regression on RTs and first saccades
+x = saccadeRT(:,1);
+firstSaccadeStatus(firstSaccadeStatus == 2) = 0; % need a binary variable 
+y = firstSaccadeStatus;
+
+figure;
+[b,dev,stats] = glmfit(x,y,'binomial','link','logit');
+yfit = glmval(b,x,'logit');
+
+xx = linspace(0,1.8);
+ yfit = glmval(b,xx,'logit');
+ plot(x,y,'o',xx,yfit,'-')
+
+
+
+
+
+
+
+
+
--- Analysis/quickMetrics.m
+++ Analysis/quickMetrics.m
@@ -9,106 +9,149 @@ prop1Session = zeros(n,1);
 prop2Session = zeros(n,1);
 prop0Session = zeros(n,1);
 firstSaccade = zeros(n,1);
-firstSaccadeCorrect = zeros(n,1);
-propMatchTime = zeros(n,1);
+firstSaccadeCorrectSession = zeros(n,1);
+propMatchTotalTime = zeros(n,1);
+firstSaccadeTimeSession = zeros(n,1);
 
-
-for i = 1:n 
-    loopTime = dv.pa.probeTime / length(PDS.data.eyeLocProbe{i});
-    
-    prop1 = length(find(PDS.data.eyeLocProbe{i} == 1)) / length(PDS.data.eyeLocProbe{i});
-    prop2 = length(find(PDS.data.eyeLocProbe{i} == 2)) / length(PDS.data.eyeLocProbe{i});
-    prop0 = length(find(PDS.data.eyeLocProbe{i} == 0)) / length(PDS.data.eyeLocProbe{i});
-    
-    prop1Time = loopTime * length(find(PDS.data.eyeLocProbe{i} == 1));
-    prop2Time = loopTime * length(find(PDS.data.eyeLocProbe{i} == 2));
-    prop0Time = loopTime * length(find(PDS.data.eyeLocProbe{i} == 0));
-    
-    if PDS.data.correctObject(i) == 1
-        %oddsMatch = prop1 / (1-prop1); % not including looking at neither
-        propMatch = prop1 / (prop1 + prop2); % shouldn't this be the other way
-        propMatchTime(i) = prop1Time;
-    elseif PDS.data.correctObject(i) == 2
-        %oddsMatch = prop2 / (1-prop2); 
-        propMatch = prop2 / (prop1 + prop2);
-        propMatchTime(i) = prop2Time;
+for i = 1:n
+    if strcmp(PDS.trialType{i}, 'test') % only for test trials
+        loopTime = dv.pa.probeTime / length(PDS.data.eyeLocProbe{i});
         
-    else
-        propMatch = NaN;
-        propMatchTime(i) = NaN;
-    end
-    
-    %oddsMatchSession(i) = oddsMatch;
-    propMatchSession(i) = propMatch;
-    prop1Session(i) = prop1;
-    prop2Session(i) = prop2;
-    prop0Session(i) = prop0;
-    
-    % first saccades
-    if find(PDS.data.eyeLocProbe{i} == 1,1) < find(PDS.data.eyeLocProbe{i} == 2,1)
-        firstSaccade(i) = 1;
-        if PDS.data.correctObject(i) == 1 && firstSaccade(i) == 1
-            firstSaccadeCorrect(i) = 1;
+        prop1 = length(find(PDS.data.eyeLocProbe{i} == 1)) / length(PDS.data.eyeLocProbe{i});
+        prop2 = length(find(PDS.data.eyeLocProbe{i} == 2)) / length(PDS.data.eyeLocProbe{i});
+        prop0 = length(find(PDS.data.eyeLocProbe{i} == 0)) / length(PDS.data.eyeLocProbe{i});
+        
+        prop1Time = loopTime * length(find(PDS.data.eyeLocProbe{i} == 1));
+        prop2Time = loopTime * length(find(PDS.data.eyeLocProbe{i} == 2));
+        prop0Time = loopTime * length(find(PDS.data.eyeLocProbe{i} == 0));
+        
+        if PDS.data.correctObject(i) == 1
+            %oddsMatch = prop1 / (1-prop1); % not including looking at neither
+            propMatch = prop1 / (prop1 + prop2);
+            propMatchTime = prop1Time / (prop1Time + prop2Time);
+            propMatchTotalTime(i) = prop1Time;
+        elseif PDS.data.correctObject(i) == 2
+            %oddsMatch = prop2 / (1-prop2);
+            propMatch = prop2 / (prop1 + prop2);
+            propMatchTime = prop2Time / (prop1Time + prop2Time);
+            propMatchTotalTime(i) = prop2Time;
         else
-            firstSaccadeCorrect(i) = 0;
+            propMatch = NaN;
+            propMatchTime = NaN;
+            propMatchTotalTime(i) = NaN;
         end
-    elseif find(PDS.data.eyeLocProbe{i} == 2,1) < find(PDS.data.eyeLocProbe{i} == 1,1)
-        firstSaccade(i) = 2;
-        if PDS.data.correctObject(i) == 2 && firstSaccade(i) == 2
-            firstSaccadeCorrect(i) = 1;
+        
+        %oddsMatchSession(i) = oddsMatch;
+        propMatchSession(i) = propMatch;
+        propMatchTimeSession(i) = propMatchTime;
+        prop1Session(i) = prop1;
+        prop2Session(i) = prop2;
+        prop0Session(i) = prop0;
+        
+        % first saccades
+        firstSaccadeTimeSession(i) = loopTime * find(PDS.data.eyeLocProbe{i} ~= 0,1);
+        
+        if find(PDS.data.eyeLocProbe{i} == 1,1) < find(PDS.data.eyeLocProbe{i} == 2,1)
+            firstSaccade(i) = 1;
+            if PDS.data.correctObject(i) == 1 && firstSaccade(i) == 1
+                firstSaccadeCorrectSession(i) = 1;
+            else
+                firstSaccadeCorrectSession(i) = 0;
+            end
+        elseif find(PDS.data.eyeLocProbe{i} == 2,1) < find(PDS.data.eyeLocProbe{i} == 1,1)
+            firstSaccade(i) = 2;
+            if PDS.data.correctObject(i) == 2 && firstSaccade(i) == 2
+                firstSaccadeCorrectSession(i) = 1;
+            else
+                firstSaccadeCorrectSession(i) = 0;
+            end
         else
-            firstSaccadeCorrect(i) = 0;
+            firstSaccadeCorrectSession(i) = NaN;
         end
     end
-
 end
+
+% Get rid of NANs
+propMatchSession = propMatchSession(~isnan(propMatchSession));
+propMatchTime = propMatchTime(~isnan(propMatchTime));
+propMatchTotalTime = propMatchTotalTime(~isnan(propMatchTotalTime));
+prop1Session = prop1Session(~isnan(prop1Session));
+prop2Session = prop2Session(~isnan(prop2Session));
+prop0Session = prop0Session(~isnan(prop0Session));
 
 %%  Plotting
 
-% PDS.trialType{}
-trialRange = [161 240];
+% Use trial range or use all
+useTrialRange = 0;
+trialRange = [161 240]; %125:164
+if useTrialRange
+    propMatchSession = propMatchSession(trialRange(1):trialRange(2));
+    propMatchTime = propMatchTime(trialRange(1):trialRange(2));
+    propMatchTotalTime = propMatchTotalTime(trialRange(1):trialRange(2));
+end
 
-propTrialsCorrect = length(find(propMatchSession(trialRange(1):trialRange(2)) > .5)) / length(propMatchSession(trialRange(1):trialRange(2)));
+% percent correct
+propTrialsCorrect = length(find(propMatchSession > .5)) / length(propMatchSession);
 
+figure;
 % Total viewing time in a session
-propNonmatchTime = dv.pa.probeTime - (propMatchTime(trialRange(1):trialRange(2)) + prop0Session(trialRange(1):trialRange(2))); %125:164
-y = [propMatchTime(trialRange(1):trialRange(2)) propNonmatchTime prop0Session(trialRange(1):trialRange(2))]; %specific indices here NOTE , propNon is alreayd specific indicies
+propNonmatchTime = dv.pa.probeTime - (propMatchTotalTime + prop0Session);
+y = [propMatchTotalTime propNonmatchTime prop0Session]; %specific indices here NOTE , propNon is alreayd specific indicies
 subplot(221), h = barwitherr(std(y),mean(y));
 title(sprintf('Total Viewing time - %s', dv.subj)) % dv.pairOrder{1,5} particular session
 set(gca,'XTickLabel',{'Matching','Non-matching','Neither'})
 set(h,'FaceColor','b');
 ylabel('Viewing Time (s)')
 subplot(222), boxplot(y)
-title(sprintf('Total Viewing time - %s', dv.subj)) 
+title(sprintf('Total Viewing time - %s', dv.subj))
 ylabel('Viewing Time (s)')
-xtix = {'Matching','Non-matching','Neither'};   
-xtixloc = [1 2 3];      
+xtix = {'Matching','Non-matching','Neither'};
+xtixloc = [1 2 3];
 set(gca,'XTickMode','auto','XTickLabel',xtix,'XTick',xtixloc);
 
 %  Proportion viewing matched vs. non-matched
-subplot(2,2,3), boxplot(propMatchSession(trialRange(1):trialRange(2))) 
+subplot(2,2,3), boxplot(propMatchSession)
 title(sprintf('Proportion of Viewing Time: matching vs. the non-matching, Percent Correct: %0.5g',propTrialsCorrect * 100))
-ylabel('Proportion of Viewing Time: matching vs. the non-matching')
-subplot(2,2,4), hist(propMatchSession(trialRange(1):trialRange(2)))
+ylabel('Relative Viewing Time: matching vs. the non-matching')
+subplot(2,2,4), hist(propMatchSession)
 g = findobj(gca,'Type','patch');
 set(g,'FaceColor','b');
-title(sprintf('Proportion of Viewing Time: matching vs. the non-matching, Percent Correct: %0.5g',propTrialsCorrect * 100))
-xlabel('Proportion of Viewing Time: matching vs. the non-matching')
+title(sprintf('Relative Viewing Time: matching vs. the non-matching, Percent Correct: %0.5g',propTrialsCorrect * 100))
+xlabel('Relative Viewing Time: matching vs. the non-matching')
 ylabel('Number of Trials')
 % mean(propMatchSession(125:164))
 % std(propMatchSession(125:164))
 % median(propMatchSession(125:164))
 
-    
-%% Across Sessions
+%% Proportion TIME viewing matched vs. non-matched (Currently redundant)
+figure;
+subplot(1,2,1), boxplot(propMatchTimeSession)
+title(sprintf('Proportion of Viewing Time: matching vs. the non-matching, Percent Correct: %0.5g',propTrialsCorrect * 100))
+ylabel('Relative Viewing Time: matching vs. the non-matching')
+subplot(1,2,2), hist(propMatchTimeSession)
+g = findobj(gca,'Type','patch');
+set(g,'FaceColor','b');
+title(sprintf('Relative Viewing Time: matching vs. the non-matching, Percent Correct: %0.5g',propTrialsCorrect * 100))
+xlabel('Relative Viewing Time: matching vs. the non-matching')
+ylabel('Number of Trials')
 
-prop0Session(isnan(propMatchTime))=[];
-propMatchTime(isnan(propMatchTime))=[];
+
+%% First Saccades
+
+medSacTime = median(sort(firstSaccadeTimeSession(firstSaccadeTimeSession~=0)));
+
+
+
+
+%% Across Sessions - not currently used
+
+prop0Session(isnan(propMatchTotalTime))=[];
+propMatchTotalTime(isnan(propMatchTotalTime))=[];
 propMatchSession(isnan(propMatchSession))=[];
 
-% Total viewing time 
-propNonmatchTime = dv.pa.probeTime - (propMatchTime + prop0Session); 
-y = [propMatchTime propNonmatchTime prop0Session]; 
+% Total viewing time
+propNonmatchTime = dv.pa.probeTime - (propMatchTotalTime + prop0Session);
+y = [propMatchTotalTime propNonmatchTime prop0Session];
 subplot(221), h = barwitherr(std(y),mean(y));
 title('Total Viewing time - Across Sessions')
 set(gca,'XTickLabel',{'Matching','Non-matching','Neither'})
@@ -117,8 +160,8 @@ ylabel('Viewing Time (s)')
 subplot(222), boxplot(y)
 title('Total Viewing time - Across Sessions')
 ylabel('Viewing Time (s)')
-xtix = {'Matching','Non-matching','Neither'};   
-xtixloc = [1 2 3];      
+xtix = {'Matching','Non-matching','Neither'};
+xtixloc = [1 2 3];
 set(gca,'XTickMode','auto','XTickLabel',xtix,'XTick',xtixloc);
 
 subplot(2,2,3), boxplot(propMatchSession) % need to remove nans here?!?!?!?!?!?!?!?!?!?
@@ -134,14 +177,14 @@ ylabel('Number of Trials')
 %%
 
 % plot(propMatchSession(125:164))
-% 
+%
 % prop1Session(125:164)
 % prop2Session(125:164)
 % prop0Session(125:164)
 
-x = [ones(length(firstSaccadeCorrect),1) firstSaccadeCorrect];
+x = [ones(length(firstSaccadeCorrectSession),1) firstSaccadeCorrectSession];
 [B,BINT,R,RINT,STATS] = regress(propMatchSession,x);
-regstats(propMatchSession,firstSaccadeCorrect)
+regstats(propMatchSession,firstSaccadeCorrectSession)
 %scatter(yhat,r)
 
 %---------------------
