61772a70cb64ea50b964b8c04541dd6c8f4cbbe6
lovegroove <ehart004@gmail.com>
2015-02-02 11:07:18 -0600

    clean up

--- PLDAPS Port/loadImages.m
+++ PLDAPS Port/loadImages.m
@@ -75,14 +75,6 @@ objectImages = dir([dv.fileInfo.objectPath '*.' dv.fileInfo.objectFormat]);
 sceneImages = dir([dv.fileInfo.scenePath '*.' dv.fileInfo.sceneFormat]);
 
 % Randomize Images and pair them
-% objectImages_mask = Shuffle(1:numel(objectImages));
-% sceneImages_mask = Shuffle(1:numel(sceneImages));
-% objectImages_mask = Shuffle(repmat(objectImages_mask, 1, length(sceneImages_mask) / length(objectImages_mask)));
-
-% Testing randperm instead because Shuffle depends on psychtoolbox (?)
-% sceneImages_mask = randperm(length(sceneImages));
-% objectImages_mask = datasample(repmat(randperm(length(objectImages)),1,(length(sceneImages) / length(objectImages))),length(sceneImages),'Replace',false);
-
 sceneImages_mask = randperm(length(sceneImages));
 objectImages_mask = repmat(randperm(length(objectImages)),1,(length(sceneImages) / length(objectImages)));
 objectImages_mask = objectImages_mask(randperm(length(objectImages_mask)));
@@ -97,13 +89,10 @@ for i = 1:length(dv.pairOrder)
 end
 
 % Proliferate pairings for many trials - 40 is the current size of a
-% stimulus set: 8 shapes, each paired with 5 scenes
-% for i = 1:dv.finish / size(dv.pairOrder,1) 
-%     dv.pairOrder = [dv.pairOrder;
-%     datasample(dv.pairOrder,size(dv.pairOrder,1),1,'replace',false)]; % should we overwrite dv.pairOrder, this was the problem!!!!!!
-% end
+% stimulus set: 8 shapes, each paired with 5 scenes (Made sure there was no
+% overwriting funny business!)
 
-pairOrderTemp = cell(numel(dv.finish),4);
+%pairOrderTemp = cell(numel(dv.finish),4);
 pairOrderTemp = datasample(dv.pairOrder,size(dv.pairOrder,1),1,'replace',false);
 for i = 1:(dv.finish / size(dv.pairOrder,1))
     pairOrderRep = datasample(dv.pairOrder,size(dv.pairOrder,1),1,'replace',false);
@@ -113,7 +102,7 @@ end
 dv.pairOrder = pairOrderTemp;
 
 % Attach Session ID
-sessionID = repmat(cellstr(dv.pref.sfile), length(dv.pairOrder), 1); % is dv.finish or the +40 going to be a problem here?
+sessionID = repmat(cellstr(dv.pref.sfile), length(dv.pairOrder), 1); 
 dv.pairOrder = horzcat(dv.pairOrder, sessionID);
 
 % Save .mat file in case you want a specfic set of pairings later
